b3a31cbe58e939ca0df1e67ca5c0f744be97c406
Jakob Löw <jakob@m4gnus.de>
2024-02-10 15:49:34 +0100

    :sparkles: extract page title to use as a markdown filename

--- PAF/convert-webpage.py
+++ PAF/convert-webpage.py
@@ -20,13 +20,27 @@ def download_scripts(output_folder):
     with open(os.path.join(output_folder, "txt-content.js"), "wb") as txt_content_file:
         txt_content_file.write(response_txt_content.content)
 
+def extract_title(html_content):
+    # Define the regex pattern to match the specified h1 element
+    pattern = re.compile(r'<h1 class="entry-title">(.*?)</h1>', re.DOTALL)
+
+    # Find the first match in the HTML content
+    match = re.search(pattern, html_content)
+
+    if match:
+        # Get the inner text
+        inner_text = match.group(1).strip()
+        return inner_text
+
+    return None  # Return None if no match is found
+
 def convert_html_to_md(html_content, md_url, md_filename):
     # Define the regex pattern to match the specified div element
     pattern = re.compile(r'<div class="entry-content">(.*?)</div><!-- \.entry-content -->', re.DOTALL)
 
     # Find the first match in the HTML content
     match = re.search(pattern, html_content)
-    
+   
     if match:
         # Get the inner HTML content
         inner_html = match.group(1)
@@ -54,10 +68,12 @@ def modify_html_files(html_folder):
                 with open(file_path, "r") as html_file:
                     content = html_file.read()
 
+                title = extract_title(content) or file
+
                 # Add script tags before </body>
                 content = content.replace("</body>", "<script src='showdown.min.js'></script><script src='txt-content.js'></script></body>")
 
-                md_name = ''.join(c for c in file if c.isalnum() or c in ". -_")
+                md_name = ''.join(c for c in title if c.isalnum() or c in ".-_ ")
                 md_path = os.path.join(root, '_content/', md_name + '.md')
                 if not os.path.exists(os.path.dirname(md_path)):
                     os.makedirs(os.path.dirname(md_path))
